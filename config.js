9b7a864b4fa698f48cdc2f7f769786acd46f6283
huynankhubo <87865145+huynankhubo@users.noreply.github.com>
2021-07-23 21:02:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hương à!',
+    introDesc: `Hạy lắng nghe
+    24 tủi rùi sao
+    còn chưa có
+    ngờ ớ yêu`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng Hương đã 24 ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnYes: 'Vẫn cứ là hư tai <33',
+    btnNo: 'Không, em gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh trốn mẹ ở đâu vại <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Yêu thì yêu mà không yêu thì xuất hiện e gặp cái thôi  <33333333',
+    mess: 'Méo 🥰. lêu 😘😘',
+    messDesc: 'lêu 24.',
+    btnAccept: ' bà chém mài',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
