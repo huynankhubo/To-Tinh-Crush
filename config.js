6521f76ce526470c99f6e4f3157a0f389354aed0
huynankhubo <87865145+huynankhubo@users.noreply.github.com>
2021-07-23 21:13:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'HƯƠNG à!',
+    introDesc: `Bói toán tìn iu`,
+    btnIntro: 'nhanh tao mợt',
+    title: 'Phảỉ chăng Hương mún coi   con đừng tìn duyn 😙',
+    desc: 'Phải chăng Hương mún coi   bao h mới có đc ngừi áy ',
+    btnYes: 'đúng đúng đúng vã quá chời ',
+    btnNo: 'xời nghị giề c mài chảnh',
+    question:'c hương muốn bói cgi nào',
+    btnReply: 'gửi',
+    reply: 'chị mài vã muoán có ny',
+    mess: 'tui bt tỏng bà nhá',
+    messDesc: 'CHÚC C HƯƠNG SỚM CÓ NG IU E RẢNH QUÁ K BT KHỊA AI.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
